64dad579e4be8b973c128c16449a533265875091
Jamiemccleave <jamiemccleave@gmail.com>
2023-05-04 16:20:33 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,78 +1,51 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
-echo
-echo "  'Upmerge Action' is using the following input:"
-echo "    - from_branch = '$INPUT_FROM_BRANCH'"
-echo "    - to_branch = '$INPUT_TO_BRANCH'"
-echo "    - user_name = 'GitHub Action : Merge'"
-echo "    - user_email = '<>'"
-echo "    - push_token = $INPUT_PUSH_TOKEN = ${!INPUT_PUSH_TOKEN}"
-echo
+# Define variables
+from_branch="$INPUT_FROM_BRANCH"
+to_branch="$INPUT_TO_BRANCH"
+push_token="${!INPUT_PUSH_TOKEN}"
+user_name="GitHub Action : Merge"
+user_email="<>"
+unique_data=(config/settings_data.json templates/) # Add any other unique files or directories as needed
 
-
-if [[ -z "${!INPUT_PUSH_TOKEN}" ]]; then
-  echo "Set the ${INPUT_PUSH_TOKEN} env variable."
+# Check if push token is set
+if [[ -z "${push_token}" ]]; then
+  echo "Please set the $INPUT_PUSH_TOKEN environment variable."
   exit 1
 fi
 
-git config --global --add safe.directory "$GITHUB_WORKSPACE"
-git remote set-url origin https://x-access-token:${!INPUT_PUSH_TOKEN}@github.com/$GITHUB_REPOSITORY.git
-git config --global user.name "$INPUT_USER_NAME"
-git config --global user.email "$INPUT_USER_EMAIL"
+# Set git config
+git config --global --add safe.directory "${GITHUB_WORKSPACE}"
+git config --global user.name "${user_name}"
+git config --global user.email "${user_email}"
+git remote set-url origin "https://x-access-token:${push_token}@github.com/${GITHUB_REPOSITORY}"
 
-set -o xtrace
+# Fetch and checkout from/to branches
+git fetch origin "${from_branch}"
+git checkout -b "${from_branch}" "origin/${from_branch}"
+git fetch origin "${to_branch}"
+git checkout -b "${to_branch}" "origin/${to_branch}"
 
-git fetch origin $INPUT_FROM_BRANCH
-(git checkout $INPUT_FROM_BRANCH && git pull origin $INPUT_FROM_BRANCH && git push origin $INPUT_FROM_BRANCH)||git checkout -b $INPUT_FROM_BRANCH origin/$INPUT_FROM_BRANCH
-
-#git log -1
-
-git fetch origin $INPUT_TO_BRANCH
-(git checkout $INPUT_TO_BRANCH && git pull origin $INPUT_TO_BRANCH)||git checkout -b $INPUT_TO_BRANCH origin/$INPUT_TO_BRANCH
-
-#git log -1
-git rev-parse --short HEAD
-hash=$(git rev-parse --short HEAD)
-
-if git merge-base --is-ancestor $INPUT_FROM_BRANCH $INPUT_TO_BRANCH; then
+# Check if merge is needed
+if git merge-base --is-ancestor "${from_branch}" "${to_branch}"; then
   echo "No merge is necessary"
   exit 0
-fi;
-
-set +o xtrace
-echo
-echo "  'Upmerge Action' is trying to merge the '$INPUT_FROM_BRANCH' branch ($(git log -1 --pretty=%H $INPUT_FROM_BRANCH))"
-echo "  into the '$INPUT_TO_BRANCH' branch ($(git log -1 --pretty=%H $INPUT_TO_BRANCH))"
-echo
-set -o xtrace
-
-# status 
-#git status 
-
-# Do the merge
-git merge --no-edit --no-commit --strategy-option theirs --allow-unrelated-histories $INPUT_FROM_BRANCH
-#git merge -m "GitHub Action: Merge Develop into France" develop
-
-git checkout $hash  config/settings_data.json
-git checkout $hash  templates/
-
-if [[ -z $(git status -s) ]]; then
-  echo "tree is clean"
-
-else
-  echo "tree is dirty, commiting changes"
-  
-  git commit -am  "GitHub Action: Merge "
-  git add config/settings_data.json
-  git add templates/
- 
 fi
-  
 
+# Merge from_branch into to_branch
+git merge --no-edit --no-commit --strategy-option theirs --allow-unrelated-histories "${from_branch}"
+for i in "${unique_data[@]}"; do
+  git checkout HEAD -- "${i}" # Restore unique files
+done
 
-
-
-# Push the branch
-git push --force origin $INPUT_TO_BRANCH
+# Check if changes were made
+if [[ -z $(git status -s) ]]; then
+  echo "Tree is clean"
+else
+  echo "Tree is dirty, committing changes"
+  git add "${unique_data[@]}"
+  git commit -m "GitHub Action: Merge ${from_branch} into ${to_branch}"
+  git push --force origin "${to_branch}"
+fi
