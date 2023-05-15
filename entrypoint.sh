8facb0cfd74c80d32642ded83705c5cf933c14c0
Jamiemccleave <jamiemccleave@gmail.com>
2023-05-15 12:23:14 +0100

    Develop (#1)
    * Update entrypoint.sh
    
    * no message
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# Display input information
 echo
 echo "  'Upmerge Action' is using the following input:"
 echo "    - from_branch = '$INPUT_FROM_BRANCH'"
@@ -11,12 +12,13 @@ echo "    - user_email = '<>'"
 echo "    - push_token = $INPUT_PUSH_TOKEN = ${!INPUT_PUSH_TOKEN}"
 echo
 
-
+# Check if push token is set
 if [[ -z "${!INPUT_PUSH_TOKEN}" ]]; then
   echo "Set the ${INPUT_PUSH_TOKEN} env variable."
   exit 1
 fi
 
+# Configure Git
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
 git remote set-url origin https://x-access-token:${!INPUT_PUSH_TOKEN}@github.com/$GITHUB_REPOSITORY.git
 git config --global user.name "$INPUT_USER_NAME"
@@ -24,22 +26,22 @@ git config --global user.email "$INPUT_USER_EMAIL"
 
 set -o xtrace
 
+# Fetch and checkout from_branch
 git fetch origin $INPUT_FROM_BRANCH
-(git checkout $INPUT_FROM_BRANCH && git pull origin $INPUT_FROM_BRANCH && git push origin $INPUT_FROM_BRANCH)||git checkout -b $INPUT_FROM_BRANCH origin/$INPUT_FROM_BRANCH
+git checkout $INPUT_FROM_BRANCH && git pull origin $INPUT_FROM_BRANCH || git checkout -b $INPUT_FROM_BRANCH origin/$INPUT_FROM_BRANCH
 
-#git log -1
-
+# Fetch and checkout to_branch
 git fetch origin $INPUT_TO_BRANCH
-(git checkout $INPUT_TO_BRANCH && git pull origin $INPUT_TO_BRANCH)||git checkout -b $INPUT_TO_BRANCH origin/$INPUT_TO_BRANCH
+git checkout $INPUT_TO_BRANCH && git pull origin $INPUT_TO_BRANCH || git checkout -b $INPUT_TO_BRANCH origin/$INPUT_TO_BRANCH
 
-#git log -1
-git rev-parse --short HEAD
+# Get the current commit hash
 hash=$(git rev-parse --short HEAD)
 
+# Check if merge is necessary
 if git merge-base --is-ancestor $INPUT_FROM_BRANCH $INPUT_TO_BRANCH; then
   echo "No merge is necessary"
   exit 0
-fi;
+fi
 
 set +o xtrace
 echo
@@ -48,31 +50,35 @@ echo "  into the '$INPUT_TO_BRANCH' branch ($(git log -1 --pretty=%H $INPUT_TO_B
 echo
 set -o xtrace
 
-# status 
-#git status 
-
-# Do the merge
+# Perform the merge
 git merge --no-edit --no-commit --strategy-option theirs --allow-unrelated-histories $INPUT_FROM_BRANCH
-#git merge -m "GitHub Action: Merge Develop into France" develop
 
-git checkout $hash  config/settings_data.json
-git checkout $hash  templates/
+# Checkout specific files from the hash
+git checkout $hash config/\*.json
+git checkout $hash templates/\*.json
+git checkout $hash sections/\*.json
+git checkout $hash locales/\*.json
 
+echo "Status Check: Post Checkout"
+git status
+
+# Check if there are changes to commit
 if [[ -z $(git status -s) ]]; then
   echo "tree is clean"
-
+  echo "--- End Script --"
+  exit 0
 else
-  echo "tree is dirty, commiting changes"
-  
-  git commit -am  "GitHub Action: Merge "
-  git add config/settings_data.json
-  git add templates/
- 
+  echo "tree is dirty, committing changes"
+  git commit -m "GitHub Action: Merge ${from_branch} into ${to_branch}"
+  git add config/\*.json
+  git add templates/\*.json
+  git add sections/\*.json
+  git add locales/\*.json
+
+  echo "Status Check: Post Push "
+
+  # Push the branch
+  git push --force origin $INPUT_TO_BRANCH
 fi
-  
 
 
-
-
-# Push the branch
-git push --force origin $INPUT_TO_BRANCH
